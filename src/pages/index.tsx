949784603f570c84cbbb14410b90cc3ced8d5a02
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-04-16 22:18:16 -0300

    feat: add estrutura da pagina sem acessibilidade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
-import type { NextPage } from "next";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className={styles.header}>
+        <Image src={} />
+      </div>
+
+      <div className={styles.nav}>
+        <a href="">{/* svg github */}</a>
+      </div>
+
+      <div className={styles.content}>
+        <h2>Desenvolvendo uma web acessível</h2>
+        <h4>
+          Protocolos e diretrizes orientam o desenvolvimento de tecnologias
+          acessiveis, mas e preciso olhar para alem de tudo isso
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ullam
+          expedita eos rerum vel sapiente voluptatem! Eaque ratione possimus
+          laborum recusandae sint aut doloremque eum alias, esse rem cum
+          nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ullam
+          dolorem, at quos facere, similique, esse nihil sit sed asperiores
+          magnam ducimus sint modi ipsam numquam labore officia. Labore, ullam.
+        </p>
+        <h3>O que e acessibilidade, afinal?</h3>
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
