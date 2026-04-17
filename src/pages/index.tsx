ed1b19176fb2a139a6e85932b6dc0beb49643528
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-04-17 11:02:55 -0300

    add landmarks a estrutura do html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,47 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.header}>
+    <>
+      <header role="banner" className={styles.header}>
         <Image src={} />
-      </div>
 
-      <div className={styles.nav}>
-        <a href="">{/* svg github */}</a>
-      </div>
+        <nav className={styles.nav}>
+          <a href="">{/* svg github */}</a>
+        </nav>
+      </header>
 
-      <div className={styles.content}>
-        <h2>Desenvolvendo uma web acessível</h2>
-        <h4>
-          Protocolos e diretrizes orientam o desenvolvimento de tecnologias
-          acessiveis, mas e preciso olhar para alem de tudo isso
-        </h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ullam
-          expedita eos rerum vel sapiente voluptatem! Eaque ratione possimus
-          laborum recusandae sint aut doloremque eum alias, esse rem cum
-          nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ullam
-          dolorem, at quos facere, similique, esse nihil sit sed asperiores
-          magnam ducimus sint modi ipsam numquam labore officia. Labore, ullam.
-        </p>
-        <h3>O que e acessibilidade, afinal?</h3>
-      </div>
-    </div>
+      <main>
+        <article className={styles.content}>
+          <header>
+            <h2>Desenvolvendo uma web acessível</h2>
+            <h4>
+              Protocolos e diretrizes orientam o desenvolvimento de tecnologias
+              acessiveis, mas e preciso olhar para alem de tudo isso
+            </h4>
+          </header>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ullam expedita eos rerum vel sapiente voluptatem! Eaque ratione
+            possimus laborum recusandae sint aut doloremque eum alias, esse rem
+            cum nostrum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ullam
+            dolorem, at quos facere, similique, esse nihil sit sed asperiores
+            magnam ducimus sint modi ipsam numquam labore officia. Labore,
+            ullam.
+          </p>
+          <h3>O que e acessibilidade, afinal?</h3>
+        </article>
+      </main>
+
+      <footer role="banner" className={styles.header}>
+        <Image src={} />
+
+        <nav className={styles.nav} aria-label="Rodape">
+          <a href="">{/* svg github */}</a>
+        </nav>
+      </footer>
+    </>
   );
 }
